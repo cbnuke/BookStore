64d3fd839041b53b856e5497be80a36fb69334d1
CBNUKE <jimmy29922@gmail.com>
2018-03-30 18:32:19 +0700

    Backup from PC

---
+++ resources/views/types/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/theme_bootstrap4')
+
+@section('page-title')
+    <h1>Pricing</h1>
+    <p>Quickly build an effective</p>
+@endsection
+
+@section('page-content')
+    <p>This is appended to the master sidebar.</p>
+@endsection
